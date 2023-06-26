f48f2bbf228296f9a5453e6c6b52238ab6834a74
Viktoriya Borisovskaya <vmiracle13@gmail.com>
2023-06-26 12:05:30 +0300

    add repo name into routes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <a className="logo-box" href="/">
+  <a className="logo-box" href="https://vmiracle13.github.io/proxysellertest/">
     <p className="m-0 logo-text">Proxy test</p>
   </a>
 );
